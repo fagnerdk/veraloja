6b443af99be5e392ae4a2c412ae74328f29d6ab6
fagnerdk <100000573+fagnerdk@users.noreply.github.com>
2022-06-02 12:55:37 -0300

    ok

---
+++ node0.js
@@ -0,0 +1,20 @@
+var valo1=0
+document.getElementById("v1").innerHTML=valo1;
+var valo2=0
+document.getElementById("v2").innerHTML=valo2;
+var valo3=0
+document.getElementById("v3").innerHTML=valo3;
+var valo4=0
+document.getElementById("v4").innerHTML=valo4;
+var valo5=0
+document.getElementById("v5").innerHTML=valo5;
+var valo6=0
+document.getElementById("v6").innerHTML=valo6;
+var valo7=0
+document.getElementById("v7").innerHTML=valo7;
+var valo8=0
+document.getElementById("v8").innerHTML=valo8;
+var valo9=0
+document.getElementById("v9").innerHTML=valo9;
+
+
