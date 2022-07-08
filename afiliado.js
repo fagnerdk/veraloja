6dd5e09becfa52a452f5be5022ea4172bdd27595
fagnerdk <100000573+fagnerdk@users.noreply.github.com>
2022-07-08 18:48:31 -0300

    OK

---
+++ afiliado.js
@@ -0,0 +1,19 @@
+//var valo1=20
+//document.getElementById("v1").innerHTML = valo1 ;
+//var valo2=20
+//document.getElementById("v2").innerHTML=valo2;
+//var valo3=20
+//document.getElementById("v3").innerHTML=valo3;
+//var valo4=20
+//document.getElementById("v4").innerHTML=valo4;
+//var valo5=10
+//document.getElementById("v5").innerHTML=valo5;
+//var valo6=20
+//document.getElementById("v6").innerHTML=valo6;
+//var valo7=20
+//document.getElementById("v7").innerHTML=valo7;
+//var valo8=20
+//document.getElementById("v8").innerHTML=valo8;
+//var valo9=20
+//document.getElementById("v9").innerHTML=valo9;
+
