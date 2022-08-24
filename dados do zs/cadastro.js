16d76dad3cc5530c69cd04a49d94bcab39e45f41
fagnerdk <100000573+fagnerdk@users.noreply.github.com>
2022-08-24 19:47:37 -0300

    ok

--- dados do zs/cadastro.js
+++ dados do zs/cadastro.js
@@ -2,15 +2,15 @@ let a1 = document.getElementById('RS').innerHTML = 20;
 function myFunction( ) {
   var x = document.getElementById("myNumber").value;
   var numer = a1;
-  document.getElementById("preço").innerHTML = x*numer ;
+ document.getElementById("preço").innerHTML = x*numer ;
  
 }
 
+
 var pga1 = '../dados do zs/KWAI.jpg';
 document.getElementById("fota1").src = pga1;
-var pga2= pga1;
+ pga2= pga1;
 document.getElementById("fota2").src = pga2;
-
 
 
 
