9d4054e73efd3776afbc2e74aee4ca7443d1704d
fagnerdk <100000573+fagnerdk@users.noreply.github.com>
2022-09-14 18:33:10 -0300

    ok

--- anucios/anucios0.js
+++ anucios/anucios0.js
@@ -4,4 +4,4 @@ document.getElementById("fota1").src = pga1;
 var pga2= pga1
 document.getElementById("fota2").src = pga2;
 
-alert('atenção este site ainda está em fase de desenvolvimento')
+//alert('atenção este site ainda está em fase de desenvolvimento')
