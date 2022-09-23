045e5f12e74485eb21a502dbe98577ac135d1a40
fagnerdk <100000573+fagnerdk@users.noreply.github.com>
2022-09-22 22:49:36 -0300

    ok

--- dados do zs/cadastro.js
+++ dados do zs/cadastro.js
@@ -1,15 +1,17 @@
-var preça = document.getElementsByClassName("RS")[0].innerHTML = "R$";
+
+
+var preço = document.getElementsByClassName("RS")[0].innerHTML = "R$";
 var preço = document.getElementById("vt").innerHTML = 22 ;
 // ide do produto pagina1
-var pg1 = document.getElementById("pg1-a1").value;
+var pg1 = document.getElementById("pg1-a1");
 var pg1 = "22"
 function myFunction(){
-var nume = document.getElementById("QTD").value
+var nume = document.getElementById("QTD").value;
 var x =  nume * preço  ;
 document.getElementById("preço").innerHTML = x;
-console.log(x)
+//console.log(x)
 }
-console.log(pg1)
+console.log(x([pga1]))
 
 
 var pga1 = '../dados do zs/KWAI.jpg';
@@ -17,6 +19,9 @@ document.getElementById("fota1").src = pga1;
  pga2= pga1;
 document.getElementById("fota2").src = pga2;
 
+import { id} from "./criando";
+var x = id;
+console.log(x())
 
 
 
---
+++ dados do zs/criando.js
@@ -0,0 +1,30 @@
+
+//fuçoes da pasta do cadstro id
+
+export var pagina1=['a','b','c','d','f','g','h','i','g','l'];
+
+let pga0 = pagina1[0]
+let pgb1 = pagina1[1]
+let pgc2 = pagina1[2]
+let pgd3 = pagina1[3]
+let pgf4 = pagina1[4]
+let pgg5 = pagina1[5]
+let pgh6 = pagina1[6]
+let pgi7 = pagina1[7]
+let pgg8 = pagina1[8]
+let pgl9 = pagina1[9]
+
+pga0 = ["a1","b2","c3"]
+pgb1 = ["a1","b2","c3"]
+pgc2 = ["a1","b2","c3"]
+pgd3 = ["a1","b2","c3"]
+pgf4 = ["a1","b2","c3"]
+pgg5 = ["a1","b2","c3"]
+pgh6 = ["a1","b2","c3"]
+pgi7 = ["a1","b2","c3"]
+pgg8 = ["a1","b2","c3"]
+pgl9 = ["a1","b2","c3"]
+
+//console.log();
+
+
